182c8a81b5dd47f7f7ea3de22abb1258aa48d5a0
jacobharden <jacobharden@kubrickgroup.com>
2023-02-08 17:46:25 +0000

    Added variable input to modular test for easy entry, moved results write external to functions

--- modular test.py
+++ modular test.py
@@ -16,29 +16,48 @@ import consistency
 import momentumanalysis
 import tradingtest
 
-pricedata = csvtoprices.readstockcsv('S&P500pricesadj.csv',
-                         '2010-12-01', '2016-12-05', "IVV", '3BMS')
-logreturns = csvtoprices.logreturn(pricedata, "IVV")
+"""Set Variables from here"""
+
+file = 'S&P500pricesadj.csv'
+startdate = '2010-12-01'
+enddate = '2016-12-05'
+ETFname = "IVV"
+interval = '3BMS'
+lookback_date = '2015-12-01'
+worstreturn = -0.15
+formation_start = '2015-06-01'
+formation_end = '2015-12-01'
+Rsquared = 0.6
+slope = 0
+use_gradient = False
+tradestartpoint = '2016-03-01'
+comparativedatafile = 'SPYpricesadj.csv'
+tradeenddate = '2016-09-01'
+returnfile = "MomentumReturnsModular.txt"
+
+pricedata = csvtoprices.readstockcsv(file, startdate, enddate, ETFname, interval)
+logreturns = csvtoprices.logreturn(pricedata, ETFname)
 
 best_performers = consistency.get_outperformers(relativereturns = logreturns['relativelogreturns'], 
-                                                lookback_date = '2015-12-01', 
-                                                worstreturn = -0.15)
+                                                lookback_date = lookback_date, 
+                                                worstreturn = worstreturn)
 
 momentum = momentumanalysis.momentumanalyse(equitylogreturns = (logreturns['logreturns'])[best_performers], 
-                                            formation_start = '2015-06-01', 
-                                            formation_end = '2015-12-01', 
-                                            Rsquared = 0.6, 
-                                            slope = 0,
-                                            use_gradient = False)
+                                            formation_start = formation_start, 
+                                            formation_end = formation_end, 
+                                            Rsquared = Rsquared, 
+                                            slope = slope,
+                                            use_gradient = use_gradient)
 
 results = tradingtest.trade(rawlogreturns=(logreturns['logreturns'])[best_performers].loc[:,momentum], 
                             comparativelogreturns = (logreturns['relativelogreturns'])[best_performers].loc[:,momentum], 
-                            startpoint = '2016-03-01', 
-                            comparativedatafile = 'SPYpricesadj.csv', 
-                            startdate = '2016-03-01', 
-                            enddate = '2016-12-01', 
-                            returnfile = "MomentumReturnsModular.txt", 
-                            worstreturn = -0.15, Rsquared = 0.6)
+                            startpoint = tradestartpoint , 
+                            comparativedatafile = comparativedatafile, 
+                            enddate = tradeenddate)
+
+with open(returnfile, "a") as results_file:
+    results_file.write(f"\n{startdate},{enddate},{tradestartpoint},{worstreturn},{Rsquared}, {use_gradient}, {results[0]},{results[1]},{results[2]}")
+#writes results to txt file in csv format
 
 #it works wooooo
 
--- tradingtest.py
+++ tradingtest.py
@@ -7,7 +7,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def trade(rawlogreturns, comparativelogreturns, startpoint, comparativedatafile, startdate, enddate, returnfile, worstreturn, Rsquared):
+def trade(rawlogreturns, comparativelogreturns, startpoint, comparativedatafile, enddate):
     """
     
 
@@ -21,16 +21,8 @@ def trade(rawlogreturns, comparativelogreturns, startpoint, comparativedatafile,
         Starting date slice for trading. REMEMBER: STARTING SLICE NEXT INTERVAL AFTER FORMATION, OTHERWISE FORESIGHT BIAS
     comparativedatafile : filepath
         file comparing data for comparison (e.g SPY).
-    startdate : DATE
-        Starting date of comparativedatafile csv to read, match with start date of your trading data.
     enddate : DATE
         Ending date of comparativedatafile csv to read, match with end date of your trading data.
-    returnfile : filepath
-        filepath to return results as csv format (appended to new row).
-    worstreturn : FLOAT
-        worstreturn parameter if consistency function used, if not fill N/A.
-    Rsquared : TYPE
-        Rsquared if consistency used, if not fill N/A.
 
     Returns
     -------
@@ -38,12 +30,12 @@ def trade(rawlogreturns, comparativelogreturns, startpoint, comparativedatafile,
 
     """
 
-    rawstratreturns=rawlogreturns[startpoint:] #log returns for hold period
+    rawstratreturns=rawlogreturns[startpoint:enddate] #log returns for hold period
     strategyreturn=np.mean(np.exp(rawstratreturns.sum()))
     plt.style.use('seaborn')
     rawstratreturns.plot(kind='line', figsize=(24, 15), title='Holding period Return selected equities')
     #plots log returns for visualization of momentum "wave"
-    benchmarkstratreturns=comparativelogreturns[startpoint:]
+    benchmarkstratreturns=comparativelogreturns[startpoint:enddate]
     benchmarkreturn=np.mean(np.exp(benchmarkstratreturns.sum()))
     #same as previous but comparing to ETF as benchmark
     
@@ -51,19 +43,15 @@ def trade(rawlogreturns, comparativelogreturns, startpoint, comparativedatafile,
     print("performance compared to benchmark in holding period:", benchmarkreturn)
     
     SPYpricedata = pd.read_csv(comparativedatafile, parse_dates = True, index_col = "Date")
-    SPYpricedata = SPYpricedata[startdate:enddate] 
+    SPYpricedata = SPYpricedata[startpoint:enddate] 
     SPYpricedata = SPYpricedata.resample("B").asfreq().ffill()
     SPYpricedata = SPYpricedata.resample("3BMS").asfreq()
     SPYpricedata.dropna(inplace=True)
     #importing SPY for pure benchmark
     SPYlogreturns=np.log(SPYpricedata/SPYpricedata.shift(1))  #calculates log returns 
-    SPYreturns = SPYlogreturns[startpoint:]  #returns from SPY over hold period
+    SPYreturns = SPYlogreturns[startpoint:enddate]  #returns from SPY over hold period
     SPYreturn = np.exp(SPYreturns.sum()) #overall SPY return
     print('performance of SPY in same period was', SPYreturn)
     returnSPY = SPYreturn.iloc[0] #otherwise junk is written into CSV because of series datatype
-    """NOTE OF RESULTS"""
-    returnlocation = r"C:/Users/jakey/Documents/Python Finance/Momentum based strats/MomentumReturnsModular.txt"
-    with open(returnfile, "a") as results_file:
-        results_file.write(f"\n{startdate},{enddate},{startpoint},{worstreturn},{Rsquared},{strategyreturn},{benchmarkreturn},{returnSPY}")
-    #writes results to txt file in csv format
-    return (strategyreturn, benchmarkreturn)
+
+    return (strategyreturn, benchmarkreturn, returnSPY)
